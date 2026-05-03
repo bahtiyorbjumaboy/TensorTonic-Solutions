88f0d3880a1d0f1f2ad42faebfd339ffdcbfdb2f
Bahtiyor Jumaboyev <167947108+bahtiyorbjumaboy@users.noreply.github.com>
2026-05-03 11:21:06 -0400

    Add solution for sql-rank-dense-rank

---
+++ study-plans/sql-basics/sql-rank-dense-rank/sql-rank-dense-rank.sql
@@ -0,0 +1,8 @@
+SELECT
+    model_name,
+    dataset,
+    accuracy,
+    RANK() OVER (PARTITION BY dataset ORDER BY accuracy DESC) AS accuracy_rank,
+    DENSE_RANK() OVER (PARTITION BY dataset ORDER BY accuracy DESC) AS accuracy_dense_rank
+FROM model_metrics
+ORDER BY 2, 3 DESC, 1
