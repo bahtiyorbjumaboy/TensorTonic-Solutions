773efecd967e2e744bf4dd33f934d4a8cb7e1c0a
Bahtiyor Jumaboyev <167947108+bahtiyorbjumaboy@users.noreply.github.com>
2026-05-06 18:06:40 -0400

    Add solution for sql-date-functions

---
+++ study-plans/sql-basics/sql-date-functions/sql-date-functions.sql
@@ -0,0 +1,9 @@
+SELECT
+    username,
+    signup_date,
+    EXTRACT(YEAR FROM signup_date) AS signup_year,
+    EXTRACT(MONTH FROM signup_date) AS signup_month,
+    EXTRACT(QUARTER FROM signup_date) AS signup_quarter,
+    DATE_TRUNC('month', signup_date) AS cohort_month,
+FROM signups
+ORDER BY 2, 1
